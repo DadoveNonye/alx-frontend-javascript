5eef5556deb3843ee8e1172aea133012d3371ab8
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-04-26 06:14:46 +0100

    Fix typo in handleResponseFromAPI function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,9 +2,10 @@ function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log("Got a response from the API");
-      return { status: 200, body: "Success" };
+      return { status: 200, body: "success" };
     })
     .catch(() => {
+      console.log("Got a response from the API");
       return new Error();
     });
 }
