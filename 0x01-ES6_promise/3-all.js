f4932a1b00e47b9546fd243ef54c1d4d7dd57c99
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-04-28 10:39:41 +0100

    Refactor handleProfileSignup function in 0x01-ES6_promise/3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,11 +3,11 @@ import { uploadPhoto, createUser } from "./utils.js";
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+      console.log(`body ${user.firstName} ${user.lastName}`);
     })
     .catch((error) => {
       console.log("Signup system offline");
     });
 }
 
-export default handleProfileSignup;
+handleProfileSignup();
