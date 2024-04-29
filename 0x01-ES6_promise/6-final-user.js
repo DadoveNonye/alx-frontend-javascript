cdfa0e175f1a33d6e5911cce5c99e82d71b8bc55
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-04-29 03:41:17 +0100

    Refactor handleProfileSignup function to use async/await and handle rejected promises in 0x01-ES6_promise/6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,18 @@
 import signUpUser from "./4-user-promise.js";
 import uploadPhoto from "./5-photo-reject.js";
 export function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPro = signUpUser(firstName, lastName);
-  const uploadpro = uploadPhoto(fileName);
+  return new Promise((resolve, reject) => {
+    const signUpPro = signUpUser(firstName, lastName);
+    const uploadpro = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPro, uploadpro]).then((data) => {
-    return data.map((datum) => ({
-      status: datum.status,
-      value: datum.status === "fulfilled" ? datum.value : datum.reason,
-    }));
+    return Promise.allSettled([signUpPro, uploadpro])
+      .then((data) => {
+        const result = data.map((datum) => ({
+          status: datum.status,
+          value: datum.status === "fulfilled" ? datum.value : datum.reason,
+        }));
+        resolve(result);
+      })
+      .catch(reject);
   });
 }
