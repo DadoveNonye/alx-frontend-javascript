f7b7fbfff29cd7341c55ccf27489e8077df69b72
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-04-30 02:40:10 +0100

    Add Airport class to 0x02-ES6_classes/7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,15 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== "string") {
+      throw TypeError("name must be a string");
+    }
+    if (typeof code !== "string") {
+      throw TypeError("code must be a string");
+    }
+    this._name = name;
+    this._code = code;
+  }
+  toString() {
+    return this._code;
+  }
+}
