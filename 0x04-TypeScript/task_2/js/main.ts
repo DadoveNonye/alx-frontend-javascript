eca5f062192d74528c8419314fe8a3333615d649
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-05-02 06:13:40 +0100

    Refactor main.ts to add executeWork function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -37,3 +37,16 @@ class Teacher implements TeacherInterface {
     return "Getting to work";
   }
 }
+function isDirector(
+  employee: DirectorInterface | TeacherInterface
+): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
